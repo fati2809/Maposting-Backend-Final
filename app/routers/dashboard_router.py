7a4912f9c38a4a90e2a967bc0ff602d6ba578501
fer21993 <2022371103@uteq.edu.mx>
2026-03-30 12:44:12 -0600

    fix: acomodar query para reportes

--- app/routers/dashboard_router.py
+++ app/routers/dashboard_router.py
@@ -103,10 +103,10 @@ async def get_reporte():
         
         # Obtener eventos con relaciones
         eventos_response = supabase.table("eventos").select("""
-            name_event, timedate_event, status_event, id_building, id_profe,
-            edificios!eventos_id_building_fkey(name_building),
-            profesor(nombre_profe)
-        """).order("timedate_event", desc=True).execute()
+                name_event, timedate_event, status_event, id_building, id_profe,
+                edificios!eventos_id_building_fkey(name_building),
+                profesor!eventos_id_profe_fkey(nombre_profe)
+            """).order("timedate_event", desc=True).execute()
         
         eventos = []
         for e in eventos_response.data:
